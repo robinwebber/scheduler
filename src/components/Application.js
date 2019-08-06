1e5ea28e051e72bd6534cbcb824e705548644883
Robin Webber <rwebber@Robins-MacBook-Pro.local>
2019-08-06 19:06:09 -0400

    added scheduler-api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import Appointment from 'components/Appointment/index'
+
 import "components/Application.scss";
 import DayList from "./DayList";
 
@@ -21,9 +23,58 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+  id: 3,
+  time: "2pm",
+  interview: {
+    student: "Billy Bill-Bob",
+    interviewer: {
+      id: 2,
+      name: "Serge Gainsbourgh",
+      avatar: "https://i.imgur.com/twYrpay.jpg",
+    }
+  }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Lucy Lamour",
+      interviewer: {
+        id: 3,
+        name: "Brigitte Bardot",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+    }
+];
+
 export default function Application(props) {
 
+  // const { appointments } = props; 
+  
+
+  const appointmentMaker =  appointments.map((appointment) => <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />)
+
   const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,7 +97,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentMaker}
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,7 +10,11 @@ export default function Appointment(props) {
   return (
     <React.Fragment >
     <Header time={props.time}/>
-    {props.interview ? <Show student={props.interview.student} onEdit={props.onEdit} onDelete={props.onDelete} interviewer={props.interviewer} /> : <Empty onAdd={props.onAdd}/>}
+    {props.interview ? 
+      <Show student={props.interview.student} onEdit={props.onEdit} onDelete={props.onDelete} interviewer={props.interview.interviewer} /> 
+    : 
+      <Empty onAdd={props.onAdd}/>
+    }
     </ React.Fragment>
   );
 }
